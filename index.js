f8eda3022e6e6d6b2f0a69458e2c8d7f7b58538f
iuriraredu <iuri.ramos.reducino@gmail.com>
2025-11-23 13:30:36 -0300

    Refatoração das funções de entrada e verificação de nível para melhorar a legibilidade e a estrutura do código.

--- index.js
+++ index.js
@@ -1,41 +1,59 @@
+const SEPARATOR = "----------------------------------\n";
+
 const readline = require("readline");
 
-const scanner = readline.createInterface({
+const input = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-startProgram();
-
-function displaysMessageAndReturnsResponse(message, callback){
-    scanner.question(message + ": ", function(response){
+function ask(question, callback){
+    input.question(question + ": ", function(response){
         callback(response);
     });
 }
 
-function checkLevel(name, xp){
+function getLevel(name, xp){
     let level = 
         xp >= 10001 ? "Radiante": 
-        xp >= 9001 ? "Imortal": 
-        xp >= 8001 ? "Ascendente": 
-        xp >= 7001 ? "Platina": 
-        xp >= 5001 ? "Ouro": 
-        xp >= 2001 ? "Prata": 
-        xp >= 1001 ? "Bronze": "Ferro";
+        xp >=  9001 ? "Imortal": 
+        xp >=  8001 ? "Ascendente": 
+        xp >=  7001 ? "Platina": 
+        xp >=  5001 ? "Ouro": 
+        xp >=  2001 ? "Prata": 
+        xp >=  1001 ? "Bronze":  
+                      "Ferro";
     return `O herói de nome ${name} está no nível de ${level}.`;
 }
 
-function startProgram(){
-    displaysMessageAndReturnsResponse("Digite o nome do herói (ou 'sair' para encerrar)", function(name){
+function main(){
+    ask("Digite o nome do herói (ou 'sair' para encerrar)", function(name){
         if(name.toLowerCase() === 'sair'){
             console.log("Programa encerrado.");
-            scanner.close();
+            console.log(SEPARATOR);
+            input.close();
             return;
+        } else {
+            ask("Digite a quantidade de XP do herói", function(xp){
+                xp = parseInt(xp);
+                if(isNaN(xp) || xp < 0){
+                    console.log("XP inválido. Por favor, insira um número válido.");
+                    console.log(SEPARATOR);
+                    return main();
+                } else {
+                    console.log(getLevel(name, parseInt(xp)));
+                    console.log(SEPARATOR);
+                    main();
+                }
+            });
         }
-        displaysMessageAndReturnsResponse("Digite a quantidade de XP do herói", function(xp){
-            console.log(checkLevel(name, parseInt(xp)));
-            console.log("----------------------------------");
-            startProgram();
-        });
     });
 }
+
+function start(){
+    console.clear();
+    console.log("=== CLASSIFICADOR DE HERÓIS ===\n");
+    main();
+}
+
+start();
